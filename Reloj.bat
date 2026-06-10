93827c12e1ce6762bbbe8ee31c4377d4f9e42410
diegodjota04 <diego.djota@gmail.com>
2026-06-10 11:36:41 -0600

    Versión 1.3

---
+++ Reloj.bat
@@ -0,0 +1,13 @@
+@echo off
+title Reloj para Pruebas - Colegio Calasanz
+:: Intentar abrir con Google Chrome en sus rutas comunes de instalacion
+if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
+    start "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --start-maximized
+) else if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
+    start "" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --start-maximized
+) else if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" (
+    start "" "%LocalAppData%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --start-maximized
+) else (
+    :: Fallback a Microsoft Edge (instalado por defecto en Windows 10/11)
+    start msedge --app="file:///%~dp0index.html" --start-maximized
+)
