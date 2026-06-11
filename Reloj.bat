796e78792e8124b6bd0c3d173d98a674ea403edf
diegodjota04 <diego.djota@gmail.com>
2026-06-11 11:10:29 -0600

    Versión 1.4 Toggle suspension. Fecha y log

--- Reloj.bat
+++ Reloj.bat
@@ -2,12 +2,12 @@
 title Reloj para Pruebas - Colegio Calasanz
 :: Intentar abrir con Google Chrome en sus rutas comunes de instalacion
 if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
-    start "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --start-maximized
+    start "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --window-size=820,950
 ) else if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
-    start "" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --start-maximized
+    start "" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --window-size=820,950
 ) else if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" (
-    start "" "%LocalAppData%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --start-maximized
+    start "" "%LocalAppData%\Google\Chrome\Application\chrome.exe" --app="file:///%~dp0index.html" --window-size=820,950
 ) else (
     :: Fallback a Microsoft Edge (instalado por defecto en Windows 10/11)
-    start msedge --app="file:///%~dp0index.html" --start-maximized
+    start msedge --app="file:///%~dp0index.html" --window-size=820,950
 )
